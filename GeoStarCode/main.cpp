695821fecdecaa37d18ac902d6a978d00234197a
cgiger <cgiger@umich.edu>
2019-02-27 00:18:26 -0500

    code files

---
+++ GeoStarCode/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  GeoStarCode
+//
+//  Created by Carmen Giger on 2/26/19.
+//  Copyright © 2019 Carmen Giger. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    cout << "sad" << endl;
+    return 0;
+}
